d0fe6bebfb6632e5000bbb23f457df55231be477
SadLife257 <99606399+SadLife257@users.noreply.github.com>
2024-12-02 15:03:42 +0700

    update category performance test

--- categories/create_category.py
+++ categories/create_category.py
@@ -2,40 +2,50 @@ from locust import HttpUser, TaskSet, task, constant
 import random
 import string
 
-create_category_id = []
+LOGIN_INFO = {
+    'admin': {
+      "email": "admin@gmail.com",
+      "password": "Admin12345678#"
+    },
+    'bao': {
+      "email": "n.bao25702@gmail.com",
+      "password": "P@ssword1"
+    },
+    'khang': {
+      "email": "khangtuhuu@gmail.com",
+      "password": "Aa123456789!"
+    }
+}
 
 def salt(size=15, chars=string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
+
+create_category_id = []
 
 class AdminBehaviour(HttpUser):
     def on_start(self):
         response = self.client.post(
             "/api/auth/login", 
-            {
-                'email': "admin@gmail.com", 
-                'password': "Admin12345678#"
-            }
+            LOGIN_INFO['admin']
         )
         self.accessToken = response.json().get('accessToken')
 
     def on_stop(self):
         response = self.client.post(
-            "/api/auth/login", 
-            {
-                'email': "admin@gmail.com", 
-                'password': "Admin12345678#"
-            }
+            "/api/auth/login",
+            LOGIN_INFO['admin']
         )
         accessToken = response.json().get('accessToken')
         headers = {'Authorization': f'Bearer {accessToken}'}
 
-        for i in create_category_id:
+        while create_category_id:
+            id = create_category_id.pop()
             self.client.delete(
-                f'/api/categories/{i}',
+                f'/api/categories/{id}',
                 headers=headers
             )
-            print('Delete', i)
-    
+            print('Delete', id)
+
     @task
     def createCategories(self):
         headers = {'Authorization': f'Bearer {self.accessToken}'}
