7241d5a050c87890d46ca6c59dae3440aca641d9
SadLife257 <99606399+SadLife257@users.noreply.github.com>
2024-12-02 21:02:39 +0700

    update category performance test

---
+++ common/utils.py
@@ -0,0 +1,20 @@
+import random
+import string
+
+LOGIN_INFO = {
+    'admin': {
+      "email": "admin@gmail.com",
+      "password": "Admin12345678#"
+    },
+    'bao': {
+      "email": "n.bao25702@gmail.com",
+      "password": "P@ssword1"
+    },
+    'khang': {
+      "email": "khangtuhuu@gmail.com",
+      "password": "Aa123456789!"
+    }
+}
+
+def salt(size=15, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
