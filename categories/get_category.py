155019a3d108603eb0ba9c2a5a4a6b4a36bff9ee
SadLife257 <99606399+SadLife257@users.noreply.github.com>
2024-12-03 14:37:41 +0700

    add category test

--- categories/get_category.py
+++ categories/get_category.py
@@ -1,31 +1,42 @@
-from locust import HttpUser, task
+from locust import HttpUser, SequentialTaskSet, task, between
 from common.utils import LOGIN_INFO
 import random
 
 class UserBehavior(HttpUser):
-    def on_start(self):
-        response = self.client.post(
-            "/api/auth/login", 
-            LOGIN_INFO['bao']
-        )
-        self.accessToken = response.json().get('accessToken')
 
+    # Execute Task by the order that they are written using SequentialTaskSet
     @task
-    def getAllCategories(self):
-        headers = {'Authorization': f'Bearer {self.accessToken}'}
+    class Flow(SequentialTaskSet):
+        # Login as user
+        @task
+        def login(self):
+            response = self.client.post(
+                "/api/auth/login", 
+                LOGIN_INFO['bao']
+            )
+            self.accessToken = response.json().get('accessToken')
 
-        response = self.client.get(
-            '/api/categories',
-            headers=headers
-        )
+        # Get category list and pick a random category ID
+        @task
+        def getAllCategories(self):
+            headers = {'Authorization': f'Bearer {self.accessToken}'}
 
-        self.category_id = random.choice(response.json()['categories'])['_id']
+            response = self.client.get(
+                '/api/categories',
+                headers=headers
+            )
 
-    @task
-    def getCategoryWithId(self):
-        headers = {'Authorization': f'Bearer {self.accessToken}'}
+            # Pick a random category from list
+            self.category_id = random.choice(response.json()['categories'])['_id']
 
-        self.client.get(
-            f'/api/categories/{self.category_id}',
-            headers=headers
-        )
+        # Get category infomation with ID
+        @task
+        def getCategoryWithId(self):
+            headers = {'Authorization': f'Bearer {self.accessToken}'}
+
+            # Group multiple unique category by one name "/categories"
+            self.client.get(
+                f'/api/categories/{self.category_id}',
+                headers=headers,
+                name='/categories'
+            )
